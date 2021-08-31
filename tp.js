3374cb2499d05f05258029665f4171053938794b
tommudano <38632202+tommudano@users.noreply.github.com>
2021-08-31 19:18:43 -0300

    Agregado de callbacks

--- tp.js
+++ tp.js
@@ -1,5 +1,6 @@
 // --- START --- : Clases de tipo Lista
 
+<<<<<<< Updated upstream
 /*abstract*/ class Lista{          
     listaProductos;
 
@@ -11,6 +12,22 @@
 }
 
 class Stock extends Lista{
+=======
+class Cliente {
+    #id;
+    nombre;
+    apellido;
+    carrito;
+    static #ultimoId = 0;
+    
+    constructor(nombre, apellido) {
+        Cliente.#ultimoId += 1;
+        this.#id = Cliente.#ultimoId;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.carrito = new Carrito();
+    }
+>>>>>>> Stashed changes
     
     constructor(){
         super();
@@ -24,8 +41,25 @@ class Stock extends Lista{
         //...
     }
 
+<<<<<<< Updated upstream
     modificarProducto(){
         //...
+=======
+    buscarProductoPorId(id) {
+        return this.carrito.buscarProducto(id, this.carrito.buscarPorId);
+    }
+
+    buscarProductoPorNombre(nombre) {
+        return this.carrito.buscarProducto(nombre, this.carrito.buscarPorNombre);
+    }
+
+    buscarProductoPorCosto(costo) {
+        return this.carrito.buscarProducto(costo, this.carrito.buscarPorCosto);
+    }
+
+    buscarProductoPorCantidad(cantidad) {
+        return this.carrito.buscarProducto(cantidad, this.carrito.buscarPorCantidad);
+>>>>>>> Stashed changes
     }
 
     actualizarStock(){
@@ -43,21 +77,88 @@ class Carrito extends Lista{
         //...
     }
 
+<<<<<<< Updated upstream
     buscarProducto(){
         //...
+=======
+    buscarPorId(producto, idBuscada) {
+        return producto.id == idBuscada;
+    }
+
+    buscarPorNombre(producto, nombreBuscado) {
+        return producto.nombreProducto.includes(nombreBuscado);
+    }
+
+    buscarPorCantidad(producto, cantidadBuscada) {
+        return producto.cantidadProducto == cantidadBuscada;
+    }
+
+    buscarPorCosto(producto, costoBuscado) {
+        return producto.costoProducto == costoBuscado;
+    }
+
+    buscarProducto(elementoBuscado, callback, flag /* opcional */) {
+        let productos = this.listaProductos.filter(producto => callback(producto, elementoBuscado));
+        return productos.length == 0 && flag == undefined ? "[-] No se han podido encontrar productos" : productos;
+>>>>>>> Stashed changes
     }
 
     modificarProducto(){
         //...
     }
 
+<<<<<<< Updated upstream
     quitarProducto(){
         //...
+=======
+    modificarProducto(id, nombre, costo) {
+        let producto = this.buscarProducto(id, this.buscarPorId)[0];
+        if (typeof(producto) == 'string') {
+            console.log(producto);
+        } else {
+            producto.nombreProducto = nombre == '' ? producto.nombreProducto : nombre;
+            producto.costoProducto = costo == '' ? producto.costoProducto : costo;
+        }
+    }
+    
+    actualizarStock(id, diferenciaStock) {
+        let producto = this.buscarProducto(id, this.buscarPorId)[0];
+        if (typeof(producto) == 'string') {
+            console.log(producto);
+        } else {
+            /* 
+                En vez de que solo se permita reducir la cantidad de artículos, cambio
+                el método para que permita toda posibilidad de cambio de stock. Ahora
+                son valores positivos para aumentar el stock y valores negativos para
+                reducirlo.
+            */ 
+
+            if (producto.cantidadProducto + diferenciaStock >= 0 ) {
+                producto.cantidadProducto += diferenciaStock;
+            } else {
+                console.log(producto.cantidadProducto + ", " + diferenciaStock);
+                console.log("[-] No hay suficientes artículos");
+            }
+        }
+    }
+    
+    mostrarStock() {
+        this.mostrarLista();
+>>>>>>> Stashed changes
     }
 }
 
 
+<<<<<<< Updated upstream
 // --- START --- : Clases de Tipo productos
+=======
+    //@Override
+    agregarProducto(productoEnCarrito) {
+
+        if(productoEnCarrito != undefined){
+            let elem = this.buscarProducto(productoEnCarrito.prod.id, this.buscarPorId, 0)[0];
+            if(elem != undefined){
+>>>>>>> Stashed changes
 
 class Producto{
     #id;
@@ -75,8 +176,16 @@ class Producto{
     get nombre(){
         return this.nombre;
     }
+<<<<<<< Updated upstream
     set nombre(nombre){
         this.nombre = nombre;
+=======
+
+    // @Override
+    buscarProducto(elementoBuscado, callback, flag /* opcional */) {
+        let productos = this.listaProductos.filter(producto => callback(producto.prod, elementoBuscado));
+        return productos.length == 0 && flag == undefined ? "[-] No se han podido encontrar productos" : productos;
+>>>>>>> Stashed changes
     }
 
     get costo(){
@@ -89,8 +198,24 @@ class Producto{
     get cantidad(){
         return this.cantidad;
     }
+<<<<<<< Updated upstream
     set cantidad(cantidad){
         this.cantidad = cantidad;
+=======
+
+    confirmarCompra (stock) {
+        this.listaProductos.forEach(producto => {
+            let idProducto = producto.prod.id;
+            let cantidadCompra = producto.cantidadAniadida;
+            let prodEnStock = stock.buscarProducto(idProducto, stock.buscarPorId)[0];
+            if (prodEnStock.cantidadProducto >= cantidadCompra) {
+                stock.actualizarStock(idProducto, -cantidadCompra);
+            } else {
+                console.log(`[-] No puede realizarse la compra del producto "${producto.prod.nombreProducto}" por falta de stock`);
+            }
+        });
+        this.listaProductos = [];
+>>>>>>> Stashed changes
     }
 
 }
@@ -186,3 +311,28 @@ class Dimensiones{
 
 // --- START --- : Programa
 
+<<<<<<< Updated upstream
+=======
+
+let producto1 = new Producto("Producto 1", 50, 30);
+let producto2 = new Producto("Producto 2", 20, 80);
+let producto3 = new Producto("Producto 3", 100, 35);
+let stock = new Stock();
+stock.agregarProductos([producto1, producto2, producto3]);
+stock.actualizarStock(3, 5);
+stock.actualizarStock(1, 50);
+
+
+
+let cliente = new Cliente("Juan", "Perez");
+cliente.agregarProducto(producto1, 80);
+stock.actualizarStock(1, -1);
+stock.mostrarStock();
+cliente.agregarProducto(producto2, 40);
+cliente.verCarrito();
+// cliente.comprar(stock);
+// cliente.verCarrito();
+// stock.mostrarStock();
+
+console.log(cliente.buscarProductoPorCosto(50));
+>>>>>>> Stashed changes
